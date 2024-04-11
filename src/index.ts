cf6fe798bdb122c1067c19b5a8f953f60998ec9c
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-04-10 21:17:37 -0300

    config: conecta com o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import 'dotenv/config'
 import express from 'express'
 import routes from './routes'
+import { AppDataSource } from './configs/databseConnection'
 
-const app = express()
-  
-app.use(express.json())
-app.use(routes)
-  
-app.listen(process.env.PORT)
+function setupAndStartServer() {
+  const app = express()
+        
+  app.use(express.json())
+  app.use(routes)
+        
+  app.listen(process.env.PORT)
+}
+
+AppDataSource.initialize()
+  .then(() => {
+    setupAndStartServer()
+  })
+  .catch((error) => {
+    console.error("Error during Data Source initialization", error)
+    process.exit(1)
+  })
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express"
 
 const routes = Router()
 
-
+routes.get('/', 
+  
+)
 
 export default routes
