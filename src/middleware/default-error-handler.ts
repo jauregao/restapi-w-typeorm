22e7a7df59a0d737a2eefd679c8d215480647a81
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-04-14 14:38:19 -0300

    feat: tratamento de erros padrão

---
+++ src/middleware/default-error-handler.ts
@@ -0,0 +1,13 @@
+import { Response } from 'express'
+
+export function defaultErrorHandler(error: Error, res: Response){
+
+  console.log(`Default error handler triggered.\n reason: ${error}`)
+
+  if(res.headersSent){
+    console.log(`Response was already being written, delegating to built-in Express error handler.`);
+    return
+  }
+
+  return res.status(500).json({ message : 'Internal server error.'})
+}
