dc503845c2f158a9dc02b25b6f619f18fbe1822b
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-04-15 12:12:18 -0300

    config: instala o cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import 'dotenv/config'
 import 'reflect-metadata'
+import cors from 'cors'
 import express from 'express'
 import routes from './routes'
 import { AppDataSource } from './configs/databseConnection'
 
 function setupAndStartServer() {
   const app = express()
-        
+  
+  app.use(cors({origin: true}))
   app.use(express.json())
+
   app.use(routes)
         
   app.listen(process.env.PORT)
