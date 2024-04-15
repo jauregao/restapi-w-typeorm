dabc3c119ff4d44e7a2f7dd580adaa20264180c7
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-04-15 16:59:48 -0300

    feat: endpoint de atualizar um curso

---
+++ src/controllers/updateCourse.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express'
+import { AppDataSource } from '../configs/databseConnection'
+import { defaultErrorHandler } from '../middleware/default-error-handler'
+import { Course } from '../models/Course'
+
+export async function updateCourse(req: Request, res: Response) {
+    
+  const params = req.params as any
+  const courseId = params.courseId
+    if(isNaN(courseId) || !courseId ) return res.status(400).json({ error: 'Course id not provided or invalid.'})
+  
+  const courseData = req.body as Course
+
+    try {
+      await AppDataSource
+        .createQueryBuilder()
+        .update(Course)
+        .set(courseData)
+        .where('id = :courseId', { courseId })
+        .execute()
+
+      return res.status(200).json({
+        message: `Course ${courseId} was updated successfully.`
+      })
+
+  } catch (error) {
+    return defaultErrorHandler(error, res)
+  }
+}
