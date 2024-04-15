3fe70125435b22e7fb5154003634f2555f8979d4
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-04-15 13:38:24 -0300

    feat: endpoint de filtrar um curso pela url

---
+++ src/controllers/getOneCourseByUrl.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express'
+import { AppDataSource } from '../configs/databseConnection'
+import { Course } from '../models/Course'
+import { defaultErrorHandler } from '../middleware/default-error-handler'
+
+export async function getOneCourseByUrl(req: Request, res: Response) {
+
+  const { courseUrl } = req.params
+  if(!courseUrl) return res.status(400).json({ error: "Course url not provided."})
+  
+    try {
+    const course = await AppDataSource
+    .getRepository(Course)
+    .findOneBy({
+      url: courseUrl
+    })
+
+    if(!course) return res.status(404).json({ error: "Course not found."})
+
+    const totalLessons = await AppDataSource
+    .getRepository('LESSONS')
+    .createQueryBuilder('lessons')
+    .where('lessons.courseId = :courseId', {
+      courseId: course.id
+    })
+    .getCount()
+
+    return res.status(200).json({
+      ...course,
+      totalLessons
+    })
+  } catch (error) {
+    return defaultErrorHandler(error, res)
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express"
 import { getAllCourses } from "./controllers/getAllCourses"
+import { getOneCourseByUrl } from "./controllers/getOneCourseByUrl"
 
 const routes = Router()
 
 routes.get('/courses', 
   getAllCourses
+)
+
+routes.get('/courses/:courseUrl', 
+  getOneCourseByUrl
 )
 
 export default routes
